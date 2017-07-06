9fa6ea5677da7893074ada764da80e3128d11f8c
Hendrik Kokocinski <hendrik.kokocinski@weltn24.de>
2017-07-06 12:39:14 +0200

    enhanced commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,9 +1,10 @@
 !/bin/sh
 BRANCH=`git branch | grep '^\*' | sed 's/.*\///g' | cut -f1,2 -d-`
 MESSAGE="[$BRANCH] $1"
-echo "Commitng and pushing with the following message:\n\n$MESSAGE\n"
+echo "Committing and pushing with the following message:\n\n\033[1;32m$MESSAGE\033[0m\n"
 
-read -p "Do you want to continue (y|n)? $ " ANSWER
+read -n1 -p "Do you want to continue (y|n)? $ " ANSWER
+echo""
 case $ANSWER in
     Y | y | "" ) git commit -am "$MESSAGE" && git push -u origin $(git rev-parse --abbrev-ref HEAD):$(git rev-parse --abbrev-ref HEAD);;
     * ) echo "Commit aborted";;
